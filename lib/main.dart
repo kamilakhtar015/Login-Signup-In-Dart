1815cfcb59cc3cf7973be0d68f888925dfddbb47
kamilakhtar015 <kamil.akhtar015@gmail.com>
2023-11-04 16:33:24 +0500

    Designing changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:login_signup/signup.dart';
 
 void main() {
   runApp(MaterialApp(
-    theme: ThemeData(scaffoldBackgroundColor: const Color(0xFFA9A9A9)),
+    // theme: ThemeData(scaffoldBackgroundColor: const Color(0xFFA9A9A9)),
     debugShowCheckedModeBanner: false,
     initialRoute: 'login',
     routes: {'login': (context) => const MySignUp()},
